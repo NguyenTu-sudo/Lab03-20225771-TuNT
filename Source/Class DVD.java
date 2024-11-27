51fd2771ba9d6da36973ebd254a2bceaab66505c
Nguyen Tu <tu74142@gmail.com>
2024-11-28 00:11:26 +0700

    Bai03

--- Source/Class DVD.java
+++ Source/Class DVD.java
@@ -8,17 +8,27 @@ public class DVD {
     private double cost;
     private String director;
     private int length;
-
-    public DVD(String id, String title, String category, double d, String director, int length) {
+    
+ //Lab3
+    public DVD(String id, String title, String category, double cost, String director, int length) {
         this.id = id;
         this.title = title;
         this.category = category;
-        this.cost = d;
+        this.cost = cost;
         this.director = director;
         this.length = length;
     }
 
-    public void play() {
+    public DVD(String oldTitle) {
+        this.title = oldTitle;
+        this.id = "Unknown";
+        this.category = "Unknown"; 
+        this.cost = 0.0; 
+        this.director = "Unknown";
+        this.length = 0;
+    }
+
+	public void play() {
         if (this.length > 0) {
             System.out.println("Playing DVD: " + this.title);
             System.out.println("DVD length: " + this.length);
@@ -32,10 +42,31 @@ public class DVD {
     }
 
     // Getters and Setters
-    public String getId() { return id; }
-    public String getTitle() { return title; }
-    public String getCategory() { return category; }
-    public double getCost() { return cost; }
-    public String getDirector() { return director; }
-    public int getLength() { return length; }
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title2) {
+        this.title = title2;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public int getLength() {
+        return length;
+    }
 }
